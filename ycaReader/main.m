003b84ffa6e01a33b04a8dc69ab58dc948bc7bc8
xyhang <929472555@qq.com>
2017-04-10 12:51:25 +0800

    Initial Commit

---
+++ ycaReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ycaReader
+//
+//  Created by yan on 2017/4/10.
+//  Copyright © 2017年 yca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
